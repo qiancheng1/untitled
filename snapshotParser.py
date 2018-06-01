197726475c7a7c2f8a30d9cdf75d92494f8c1866
qiancheng1 <822057651@qq.com>
2018-06-01 11:33:08 +0800

    add sys exit

--- snapshotParser.py
+++ snapshotParser.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+
 import argparse
 import json
 import os
 import sys
 import xml.sax
-
-from numpy.f2py.crackfortran import requiredpattern
 
 
 class SnapshotParser(xml.sax.ContentHandler):
@@ -28,10 +27,10 @@ class SnapshotParser(xml.sax.ContentHandler):
         if self.CurrentData == 'project':
             # print('project name %s' % self.name)
             branch_info = json.dumps(self.name)
-            if not os.path.isfile('./info.txt'):
-                with open('./info.txt', 'a') as f:
-                    f.write(branch_info)
-                    f.write('\n')
+            # if not os.path.isfile('./info.txt'):
+            with open('./info.txt', 'a') as f:
+                f.write(branch_info)
+                f.write('\n')
         self.CurrentData = ''
         self.name = ''
         self.path = ''
@@ -39,52 +38,56 @@ class SnapshotParser(xml.sax.ContentHandler):
     def characters(self, content):
         pass
 
-    def get_info(self,obranch,nbranch):
+    def get_info(self, obranch, nbranch):
         with open('./info.txt', 'r') as fr:
             for line in fr:
                 project = json.loads(line)
-                print(project,obranch,nbranch)
+                print(project, obranch, nbranch)
                 # ssh -p 29418 10.0.30.9 gerrit create-branch "$project" "$new_branch_name" "old_branch"
-                os.popen('ssh -p 29418 10.0.30.9 gerrit create-branch "%s" "%s" "%s"' % (project,obranch,nbranch))
+                os.popen('ssh -p 29418 10.0.30.9 gerrit create-branch "%s" "%s" "%s"' % (project,nbranch,obranch))
         fr.close()
         os.remove('./info.txt')
 
 
 if __name__ == '__main__':
+
     parser = xml.sax.make_parser()
     parser.setFeature(xml.sax.handler.feature_namespaces, 0)
 
     snapshot_handler = SnapshotParser()
     parser.setContentHandler(snapshot_handler)
 
-    parser.parse('snapshot.xml')
-
-    # 创建一个命令解析器的句柄
+    # Create a handle to start command parser
     mArgsParser = argparse.ArgumentParser()
+    mArgsParser.add_argument(
+        '--snapshot',
+        help='type a exist snapshot at the same path with this script',
+        required=True
+    )
     mArgsParser.add_argument(
         '--new_branch',
         help='type a new branch name',
-        required = True
+        required=True
     )
     mArgsParser.add_argument(
         '--old_branch',
         help='type a old branch name',
-        required = True
+        required=True
     )
 
     args = mArgsParser.parse_args()
-    print(args)
-    if args.new_branch is not None:
-        mNewbranch = args.new_branch
-        print(mNewbranch)
+    if args.snapshot is not None:
+        mSnapshot = args.snapshot
     else:
-        print('new branch is invalid')
+        print('snapshot is invalid')
         sys.exit(1)
-    if args.old_branch is not None:
+    if args.old_branch and args.new_branch is not None:
+        mNewbranch = args.new_branch
         mOldbranch = args.old_branch
-        print(mOldbranch)
+        print("new branch: %s, old branch: %s" % (mNewbranch, mOldbranch))
     else:
-        print('old branch is invalid')
+        print('branch info is invalid')
         sys.exit(1)
 
-    snapshot_handler.get_info(mNewbranch,mOldbranch)
+    parser.parse(mSnapshot)
+    snapshot_handler.get_info(mNewbranch, mOldbranch)

569723bb77a88a38b20115daa658c4f5bffd6db8
qiancheng1 <822057651@qq.com>
2018-07-31 16:43:51 +0800

    20180731 socket update

--- day31/服务端.py
+++ day31/服务端.py
@@ -1,12 +1,14 @@
 import socket
-
+# http://www.cnblogs.com/linhaifeng/articles/6129246.html#_label4
 phone = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+# 显示为timeout就用这个方法来解决
+phone.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
 phone.bind(("127.0.0.1",8080))
 
 phone.listen(3)
 
 print("--->")
-conn,addr = phone.accept()
+conn,addr = phone.accept() # 触发三次握手
 
 
 msg = conn.recv(1024)
@@ -14,5 +16,8 @@ print("接收到的信息是",msg)
 
 conn.send(msg.upper())
 
-conn.close()
+conn.close() # 触发四次挥手
 phone.close()
+
+# backlog 叫半链接池
+# 三次握手发生在accept()阶段,剩下的就是传输了.已经建立了可靠链接
